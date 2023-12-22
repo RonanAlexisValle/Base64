c2f616aa713cd4f580fa197b340b138badbee386
Ronan Alexis T. Valle <ronanalexistvalle@iskolarngbayan.pup.edu.ph>
2023-12-22 22:59:57 +0800

    Trial Push

---
+++ script.js
@@ -0,0 +1,38 @@
+const highPriorityData = [
+    "ronan",
+    "march 11 2004",
+    "jonel123",
+    "justine",
+    "october 28 2003",
+    "camilleganda",
+    "cams",
+    "february 06 2003",
+    "ikawparin",
+    "tagaro"
+  ];
+
+const mediumPriorityData = [
+    "black",
+    "adobo",
+    "0311",
+    "blue",
+    "sinigang",
+    "1234",
+    "white",
+    "ginisa",
+    "9876",
+    "indigo"
+  ];
+
+const lowPriorityData = [
+    "furreza",
+    "comedy",
+    "rose",
+    "hotdog",
+    "horror",
+    "tulips",
+    "rida",
+    "thriller",
+    "sunflower",
+    "water"
+  ];
